d7eb6aea0e8ecf887c1234ca753971deb4ffee7c
iuliapurcaru <202502061@post.au.dk>
2026-04-07 20:26:17 +0200

    changed the port for the backend

--- backend/utils/remove_background.py
+++ backend/utils/remove_background.py
@@ -1,8 +1,16 @@
 from transformers import pipeline
 from PIL import Image
 
-pipe = pipeline("image-segmentation", model="briaai/RMBG-1.4", trust_remote_code=True)
+pipe = None
+
+def get_pipe():
+    global pipe
+    if pipe is None:
+        # Load the model lazily
+        pipe = pipeline("image-segmentation", model="briaai/RMBG-1.4", trust_remote_code=True)
+    return pipe
 
 def remove_background(image: Image.Image) -> Image.Image:
-    result = pipe(image)
-    return result
+    p = get_pipe()
+    result = p(image)
+    return result
